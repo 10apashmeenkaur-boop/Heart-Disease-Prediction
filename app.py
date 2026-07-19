7a19847a3b36bd026dab8ea88201a8b995c2389a
Pashmeen Kaur <10a.pashmeenkaur@gmail.com>
2026-07-20 00:35:08 +0530

    Prepare project for deployment

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def home():
         return render_template("index.html")
 
 # Execution begins
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
